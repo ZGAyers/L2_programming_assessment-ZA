35a2ba47cc397cd41933276641622a3e89e3b108
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-06-04 09:52:34 +1200

    EoDW - Unfinished component 02
    EoDW = End of Days Work

---
+++ 02_get_item.py
@@ -0,0 +1,20 @@
+# Component 02 - Get item, cost, and weight from user
+
+
+# Not Blank function goes here
+def not_blank(question, error_msg, ):
+    error = error_msg
+
+    valid = False
+    while not valid:
+        response = input(question)
+
+        # If response is blank, question is repeated (loop starts over)
+        if response == "":
+            print(error)
+            continue
+
+        # if response is not blank, program continues
+        else:
+            return response
+
