1872df50b6d91807381fd7fd4834737aa59166db
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-06-08 12:42:54 +1200

    Finished component 02
    Component 02 now works as expected and prints out correctly formatted list

--- 02_get_item.py
+++ 02_get_item.py
@@ -18,3 +18,48 @@ def not_blank(question, error_msg, ):
         else:
             return response
 
+
+# Number Checking Function goes here
+def num_check(question):
+
+    error = "Please enter a number that is more than zero"
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+
+            if response <= 0:
+
+                print(error)
+            else:
+                return response
+        except ValueError:
+
+            print(error)
+
+
+# main routine
+
+# set up lists
+printout = []
+
+stop = ""
+while stop != "exit":
+    product = []
+    get_product = not_blank("Product: ",
+                            "Please fill in this field or type 'exit' to quit")
+    product.append(get_product)
+
+    # if user enters exit code break loop
+    if get_product == "exit":
+        break
+
+    get_cost = num_check("Cost: $")
+
+    get_weight = num_check("Weight(g): ")
+
+    # append the information the user entered into a single line of code
+    printout.append("${:.2f} {}, {:.2f}g".format(get_cost, get_product.title(), get_weight))
+
+for item in printout:
+    print(item)
